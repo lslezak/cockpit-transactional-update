dbad55ea4b9ca059ba9a6553819d8eb1da6495e5
Imobach González Sosa <igonzalezsosa@suse.com>
2020-11-16 12:05:47 +0000

    Add a snapshots list with dummy data

---
+++ src/components/SnapshotsList.js
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) [2020] SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact SUSE LLC.
+ *
+ * To contact SUSE LLC about this file by physical or electronic mail, you may
+ * find current contact information at www.suse.com.
+ */
+
+import React, { useState } from 'react';
+import { Card, CardHeader, CardBody, CardTitle } from '@patternfly/react-core';
+import {
+    Table,
+    TableHeader,
+    TableBody
+} from '@patternfly/react-table';
+import cockpit from 'cockpit';
+
+const _ = cockpit.gettext;
+
+const buildRows = (snapshotsList) => (
+    snapshotsList.map((snapshot) => (
+        {
+            rowKey: snapshot.id,
+            cells: [
+                snapshot.id,
+                snapshot.date.toLocaleString()
+            ],
+            active: snapshot.active,
+            current: snapshot.current
+        }
+    ))
+);
+
+export function SnapshotsList({ snapshots, onRebootRequest, onRollbackRequest }) {
+    const [rows] = useState(buildRows(snapshots));
+
+    const columns = [
+        _("Number"), _("Date")
+    ];
+
+    const actionResolver = ({ id, active, current }) => {
+        if (active && current) {
+            return null;
+        }
+
+        if (!current) {
+            return [{ title: _('Rollback'), onClick: onRollbackRequest }];
+        } else {
+            return [{ title: _('Reboot'), onClick: onRebootRequest }];
+        }
+    };
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>{_("Existing Snapshots")}</CardTitle>
+            </CardHeader>
+            <CardBody>
+                <Table
+                    aria-label={_("Existing Snapshots")}
+                    cells={columns}
+                    rows={rows}
+                    variant="compact"
+                    actionResolver={actionResolver}
+                >
+                    <TableHeader />
+                    <TableBody />
+                </Table>
+            </CardBody>
+        </Card>
+    );
+}
--- src/components/SnapshotsTab.js
+++ src/components/SnapshotsTab.js
@@ -19,24 +19,62 @@
  * find current contact information at www.suse.com.
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
     PageSection,
     PageSectionVariants
 } from '@patternfly/react-core';
+import { Loading } from './Loading';
+import { SnapshotsList } from './SnapshotsList';
 import cockpit from 'cockpit';
 
 const _ = cockpit.gettext;
+const n_ = cockpit.ngettext;
+
+/**
+ * Dummy data. Just replace with data from the D-Bus service.
+ */
+const dummySnapshots = [
+    { id: 2, date: new Date('2020-11-16 10:02') },
+    { id: 1, date: new Date('2020-11-15 14:30'), current: true },
+    { id: 0, date: new Date('2020-11-10 12:00'), active: true }
+];
 
 export function SnapshotsTab() {
+    const [loading] = useState(false);
+    const [snapshots] = useState(dummySnapshots);
+
+    const statusText = () => {
+        if (loading) {
+            return _('Loading existing snapshots. Please, wait...');
+        } else {
+            return n_('1 snapshot found', `${snapshots.length} snapshots found`, snapshots.length);
+        }
+    };
+
+    const content = () => {
+        if (loading) {
+            return <Loading />;
+        }
+
+        return (
+            <SnapshotsList
+                snapshots={snapshots}
+                onRollbackRequest={(event, id) => console.log('Rolling back to snapshot', id)}
+            />
+        );
+    };
+
     return (
         <>
             <PageSection className="content-header-extra">
                 <div id="state" className="content-header-extra--state">
-                    {_('Loading existing snapshots. Please, wait...')}
+                    { statusText() }
                 </div>
             </PageSection>
-            <PageSection variant={PageSectionVariants.light} />
+            <PageSection variant={PageSectionVariants.light}>
+                { content() }
+            </PageSection>
         </>
     );
 }
